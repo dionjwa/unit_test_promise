1d2898bece207f3e78ed9ad5b8dd5df56def0cd1
Dion Amago <dion@transition9.com>
2015-05-18 10:56:16 -0700

    Add assert statement.

--- src/haxe/unit/async/PromiseTest.hx
+++ src/haxe/unit/async/PromiseTest.hx
@@ -4,6 +4,13 @@ import promhx.Promise;
 
 class PromiseTest
 {
+	public function assertTrue(expression :Bool, ?info :Dynamic) :Void
+	{
+		if (!expression) {
+			throw info != null ? info : "Failed assertion";
+		}
+	}
+
 	public function setup() :Promise<Bool>
 	{
 		return Promise.promise(true);
