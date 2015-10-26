10d547e5e00de1427a0530dead20f89df0ffe9d9
Dion Amago <dion@transition9.com>
2015-10-26 10:41:54 -0700

    Return assert results for less lines of code when writing promise tests.

--- src/haxe/unit/async/PromiseTest.hx
+++ src/haxe/unit/async/PromiseTest.hx
@@ -14,44 +14,50 @@ import promhx.base.AsyncBase;
 
 class PromiseTest
 {
-	public function assertTrue(expression :Bool, ?posInfos :haxe.PosInfos) :Void
+	public function assertTrue(expression :Bool, ?posInfos :haxe.PosInfos)
 	{
 		if (!expression) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion';
 		}
+		return expression;
 	}
 
-	public function assertFalse(expression :Bool, ?posInfos :haxe.PosInfos) :Void
+	public function assertFalse(expression :Bool, ?posInfos :haxe.PosInfos)
 	{
 		assertTrue(!expression, posInfos);
+		return !expression;
 	}
 
-	public function assertEquals<T>(val1 :T, val2 :T, ?posInfos :haxe.PosInfos) :Void
+	public function assertEquals<T>(val1 :T, val2 :T, ?posInfos :haxe.PosInfos)
 	{
 		if (val1 != val2) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} != ${val2}';
 		}
+		return val1 == val2;
 	}
 
-	public function assertNotEquals(val1 :Dynamic, val2 :Dynamic, ?posInfos :haxe.PosInfos) :Void
+	public function assertNotEquals(val1 :Dynamic, val2 :Dynamic, ?posInfos :haxe.PosInfos)
 	{
 		if (val1 == val2) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} == ${val2}';
 		}
+		return val1 != val2;
 	}
 
-	public function assertIsNull(val1 :Dynamic, ?posInfos :haxe.PosInfos) :Void
+	public function assertIsNull(val1 :Dynamic, ?posInfos :haxe.PosInfos)
 	{
 		if (val1 != null) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} != null';
 		}
+		return val1 == null;
 	}
 
-	public function assertNotNull(val1 :Dynamic, ?posInfos :haxe.PosInfos) :Void
+	public function assertNotNull(val1 :Dynamic, ?posInfos :haxe.PosInfos)
 	{
 		if (val1 == null) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} == null';
 		}
+		return val1 != null;
 	}
 
 	public function setup() :Null<Promise<Bool>>
