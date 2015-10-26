5797977daddb725c25c5b123bcaa3ca8244c1019
Dion Amago <dion@transition9.com>
2015-10-26 10:46:42 -0700

    Type the returns

--- src/haxe/unit/async/PromiseTest.hx
+++ src/haxe/unit/async/PromiseTest.hx
@@ -14,7 +14,7 @@ import promhx.base.AsyncBase;
 
 class PromiseTest
 {
-	public function assertTrue(expression :Bool, ?posInfos :haxe.PosInfos)
+	public function assertTrue(expression :Bool, ?posInfos :haxe.PosInfos) :Bool
 	{
 		if (!expression) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion';
@@ -22,13 +22,13 @@ class PromiseTest
 		return expression;
 	}
 
-	public function assertFalse(expression :Bool, ?posInfos :haxe.PosInfos)
+	public function assertFalse(expression :Bool, ?posInfos :haxe.PosInfos) :Bool
 	{
 		assertTrue(!expression, posInfos);
 		return !expression;
 	}
 
-	public function assertEquals<T>(val1 :T, val2 :T, ?posInfos :haxe.PosInfos)
+	public function assertEquals<T>(val1 :T, val2 :T, ?posInfos :haxe.PosInfos) :Bool
 	{
 		if (val1 != val2) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} != ${val2}';
@@ -36,7 +36,7 @@ class PromiseTest
 		return val1 == val2;
 	}
 
-	public function assertNotEquals(val1 :Dynamic, val2 :Dynamic, ?posInfos :haxe.PosInfos)
+	public function assertNotEquals(val1 :Dynamic, val2 :Dynamic, ?posInfos :haxe.PosInfos) :Bool
 	{
 		if (val1 == val2) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} == ${val2}';
@@ -44,7 +44,7 @@ class PromiseTest
 		return val1 != val2;
 	}
 
-	public function assertIsNull(val1 :Dynamic, ?posInfos :haxe.PosInfos)
+	public function assertIsNull(val1 :Dynamic, ?posInfos :haxe.PosInfos) :Bool
 	{
 		if (val1 != null) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} != null';
@@ -52,7 +52,7 @@ class PromiseTest
 		return val1 == null;
 	}
 
-	public function assertNotNull(val1 :Dynamic, ?posInfos :haxe.PosInfos)
+	public function assertNotNull(val1 :Dynamic, ?posInfos :haxe.PosInfos) :Bool
 	{
 		if (val1 == null) {
 			throw '${posInfos.className}.${posInfos.methodName}:${posInfos.lineNumber} Failed assertion: ${val1} == null';
